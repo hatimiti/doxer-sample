7b99daa8a5775e7e63e052501427b08f2ce9d528
hatimiti <hatti33@gmail.com>
2015-08-24 23:01:55 +0900

    Cache機構のサンプル追加

--- doxer-sample/src/main/java/org/doxer/app/SysConfig.java
+++ doxer-sample/src/main/java/org/doxer/app/SysConfig.java
@@ -2,11 +2,15 @@ package org.doxer.app;
 
 import org.doxer.app.base.thymeleaf.util.JUtilityDialect;
 import org.doxer.xbase.config.DoxSysConfig;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
+@EnableCaching
 public class SysConfig extends DoxSysConfig {
 
 	@Bean
@@ -15,6 +19,15 @@ public class SysConfig extends DoxSysConfig {
 		SpringTemplateEngine templateEngine = super.templateEngine();
 		templateEngine.addDialect(new JUtilityDialect());
 		return templateEngine;
+	}
+
+	/**
+	 * Cache Manual
+	 * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/cache.html
+	 */
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager("sampleData");
 	}
 
 }
--- doxer-sample/src/main/java/org/doxer/app/db/dbflute/exbhv/TcmSampleBhv.java
+++ doxer-sample/src/main/java/org/doxer/app/db/dbflute/exbhv/TcmSampleBhv.java
@@ -4,6 +4,7 @@ import org.dbflute.cbean.result.ListResultBean;
 import org.doxer.app.base.type.form.hello.Val;
 import org.doxer.app.db.dbflute.bsbhv.BsTcmSampleBhv;
 import org.doxer.app.db.dbflute.exentity.TcmSample;
+import org.springframework.cache.annotation.Cacheable;
 
 /**
  * The behavior of TCM_SAMPLE.
@@ -16,6 +17,7 @@ import org.doxer.app.db.dbflute.exentity.TcmSample;
 @org.springframework.stereotype.Component("tcmSampleBhv")
 public class TcmSampleBhv extends BsTcmSampleBhv {
 
+	@Cacheable("sampleData")
 	public ListResultBean<TcmSample> findBySampleName(Val name) {
 		return this.selectList(cb -> {
 			cb.query().setSampleName_Equal(name.getVal());
